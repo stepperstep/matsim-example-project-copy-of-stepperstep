fa69a4c5d5a152fa44ec07c6166f34933e5e2b67
stepperstep <stefan.schulz.94@gmail.com>
2020-06-08 21:16:19 +0200

    first lecture second lecture third lecture fourth lecture fifth lecture sixth lecture

--- src/main/java/org/matsim/project/RunScoringExample.java
+++ src/main/java/org/matsim/project/RunScoringExample.java
@@ -3,6 +3,7 @@ package org.matsim.project;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
 import org.matsim.core.scenario.ScenarioUtils;
 
 public class RunScoringExample {
